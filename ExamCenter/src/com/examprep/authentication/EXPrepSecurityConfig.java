620410dcf0ad6aebc46ced2efa0e821175d6c69f
Kartik <rama354@gmail.com>
2014-10-30 01:07:05 +0530

    spring security

---
+++ ExamCenter/src/com/examprep/authentication/EXPrepSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.examprep.authentication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class EXPrepSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login");
+    }
+}
