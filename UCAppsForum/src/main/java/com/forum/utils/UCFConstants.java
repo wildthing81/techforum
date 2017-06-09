8151a52a387f64f1509215df711702b0b1bedd72
Ramaswamy R <r79@avaya.com>
2017-06-10 00:06:47 +0530

    user activity as server sent events

--- UCAppsForum/src/main/java/com/forum/utils/UCFConstants.java
+++ UCAppsForum/src/main/java/com/forum/utils/UCFConstants.java
@@ -5,4 +5,5 @@ public final class UCFConstants {
 	public static final String FILL_BLANKS="FILLBLANKS";
 	public static final String BINARY_CHOICE="BINARYCHOICE";
 
+	public static final int USR_ACTV_REFRESH_PERIOD=5000;
 }
